09df0c8939638a0d6de90fc4a980b337b8f7c051
msosa <marcossosa011@gmail.com>
2021-12-02 22:13:54 -0300

    added config database

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const morgan = require("morgan");
 
 const { ProductRoutes, UserRoutes } = require("./routes");
 
 const app = express();
+
+// Database
+mongoose
+  .connect("mongodb://localhost:27017/mydb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to database"))
+  .catch((err) => console.log(err));
 
 // Middlewares
 app.use(express.json());
