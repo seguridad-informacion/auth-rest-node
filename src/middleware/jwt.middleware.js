32a0eee0e230ea9d2bdb95f78a0fc5bb042ed258
msosa <marcossosa011@gmail.com>
2021-12-04 13:37:24 -0300

    added token middleware

---
+++ src/middleware/jwt.middleware.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+async function verifyToken(req, res, next) {
+  const token = req.headers["token"];
+
+  if (!token) {
+    res.status(400).json({
+      error: "Token must be sent",
+    });
+  }
+
+  jwt.verify(token, "my-secret", (error) => {
+    if (error) {
+      res.status(401).json({
+        error: "Invalid token",
+      });
+    }
+    next();
+  });
+}
+
+module.exports = verifyToken;
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,22 +1,24 @@
+const verifyToken = require('../middleware/jwt.middleware');
+
 const Router = require('express').Router();
 
-Router.get('/api/products', (req, res) => {
+Router.get('/api/products', verifyToken, (req, res) => {
   res.send("get all products");
 });
 
-Router.get("/api/products/:id", (req, res) => {
+Router.get("/api/products/:id", verifyToken, (req, res) => {
   res.send("get product by id");
 });
 
-Router.post("/api/products", (req, res) => {
+Router.post("/api/products", verifyToken, (req, res) => {
   res.send("create product");
 });
 
-Router.delete("/api/products/:id", (req, res) => {
+Router.delete("/api/products/:id", verifyToken, (req, res) => {
   res.send("delete product");
 });
 
-Router.put("/api/products/:id", (req, res) => {
+Router.put("/api/products/:id", verifyToken, (req, res) => {
   res.send("update product");
 });
 
